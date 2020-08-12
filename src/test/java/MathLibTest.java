c9896afe3898d49da60980c8a4b016958c77a636
Ben Schellenberger <ben.schellenberger@qhrtech.com>
2020-08-11 20:11:14 -0700

    another commit

--- src/test/java/MathLibTest.java
+++ src/test/java/MathLibTest.java
@@ -23,7 +23,7 @@ public class MathLibTest {
 
     @Test()
     public void test_multiplication() {
-        Assert.assertEquals( 36, underTest.multiplyNumbers( 6, 6 ) );
+        Assert.assertEquals( 24, underTest.multiplyNumbers( 12, 2 ) );
     }
 
     @Test()
